d4b56ebd10fd06b8fb31eda5a5d31a31948fa233
shibe23 <shibe23.info@gmail.com>
2021-01-17 23:45:26 +0900

    組み合わせ

--- app/Main.hs
+++ app/Main.hs
@@ -7,9 +7,15 @@ anyChar(x:xs) = (x, xs)
 test1 xs0 =
   let (x1, xs1) = anyChar xs0
       (x2, xs2) = anyChar xs1
-  in [x1, x2]
+  in ([x1, x2], xs2)
 
+test2 xs0 =
+  let (x1, xs1) = test1 xs0
+      (x2, xs2) = anyChar xs1
+  in (x1 ++ [x2], xs2)
 
 main :: IO ()
 main = do
   print $ anyChar "abt"
+  print $ test1 "abt"
+  print $ test2 "abt"

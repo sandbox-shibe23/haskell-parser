b3a60b9dfd8b2231defc9471bd583665a6a12eae
shibe23 <shibe23.info@gmail.com>
2021-01-27 22:42:13 +0900

    組み合わせ判定

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Lib
 import Control.Exception
-import Data.Char (isDigit)
+import Data.Char (isDigit, isLetter)
 
 parseTest p s = do
     print $ fst $ p s
@@ -12,9 +12,19 @@ parseTest p s = do
 anyChar(x:xs) = (x, xs)
 satisfy f (x:xs) | f x = (x, xs)
 
+char c = satisfy (== c)
+digit = satisfy isDigit
+letter = satisfy isLetter
+
+test3 xs0 =
+  let (x1, xs1) = letter xs0
+      (x2, xs2) = digit xs1
+      (x3, xs3) = digit xs2
+  in ([x1, x2, x3], xs3)
+
 main :: IO ()
 main = do
-  parseTest (satisfy (=='a')) "abt"
-  parseTest (satisfy (=='a')) "123"
-  parseTest (satisfy isDigit) "abt"
-  parseTest (satisfy isDigit) "123"
+  parseTest test3 "abc"
+  parseTest test3 "123"
+  parseTest test3 "a23"
+  parseTest test3 "a234"

e4c2cb7db11f348a7f3b4bec7c01323e28945edd
shibe23 <shibe23.info@gmail.com>
2021-01-31 23:22:39 +0900

    モナドなしに戻す

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,34 @@
-module Main where
-
-import Lib
 import Control.Exception
-import Control.Monad.State
 import Data.Char
 
 parseTest p s = do
-    print $ evalState p s
+    print $ fst $ p s
     `catch` \(SomeException e) ->
-      putStr $ show e
+        putStr $ show e
 
-anyChar = state anyChar where
-  anyChar (x:xs) = (x, xs)
-
-satisfy :: (Char -> Bool) -> State String Char
-satisfy f = state satisfy where
-  satisfy (x:xs) | f x = (x, xs)
+anyChar   (x:xs)       = (x, xs)
+satisfy f (x:xs) | f x = (x, xs)
 
 char c = satisfy (== c)
-digit = satisfy isDigit
+digit  = satisfy isDigit
 letter = satisfy isLetter
 
-test1 = sequence [anyChar, anyChar]
-test2 = (++) <$> test1 <*> sequence [anyChar]
-test3 = sequence [letter, digit, digit]
+test1 xs0 =
+    let (x1, xs1) = anyChar xs0
+        (x2, xs2) = anyChar xs1
+    in ([x1, x2], xs2)
 
-main :: IO ()
+test2 xs0 =
+    let (x1, xs1) = test1   xs0
+        (x2, xs2) = anyChar xs1
+    in (x1 ++ [x2], xs2)
+
+test3 xs0 =
+    let (x1, xs1) = letter xs0
+        (x2, xs2) = digit  xs1
+        (x3, xs3) = digit  xs2
+    in ([x1, x2, x3], xs3)
+
 main = do
     parseTest anyChar "abc"
     parseTest test1   "abc"

6efe33db8acfe4f924f04f14a18e137deec973b3
shibe23 <shibe23.info@gmail.com>
2021-02-06 22:50:11 +0900

    Parsecに置き換え

--- app/Main.hs
+++ app/Main.hs
@@ -1,33 +1,8 @@
+import Text.Parsec
 import Control.Applicative ((<$>), (<*>))
 import Control.Monad
 import Control.Monad.State
 import Data.Char
-
-parseTest p s = case evalStateT p s of
-    Right r -> print r
-    Left  e -> putStrLn $ "[" ++ show s ++ "]" ++ e
-
-anyChar = StateT $ anyChar where
-  anyChar (x:xs) = Right(x, xs)
-  anyChar _      = Left "too short"
-
-satisfy f = StateT $ satisfy where
-  satisfy (x:xs) | not $ f x = Left $ "; " ++ show x
-  satisfy xs                 = runStateT anyChar xs
-
-(StateT a) <|> (StateT b) = StateT $ \s ->
-    (a s) <|> (b s) where
-    Left a <|> Left b = Left $ b ++ a
-    Left _ <|> b      = b
-    a      <|> _      = a
-
-left = lift . Left
-
-char c = satisfy (== c)   <|> left ("not char" ++ show c)
-digit  = satisfy isDigit  <|> left "not digit"
-letter = satisfy isLetter <|> left "not letter"
-
-many p = ((:) <$> p <*> many p) <|> return [] 
 
 test1 = sequence [anyChar, anyChar]
 test2 = (++) <$> test1 <*> sequence [anyChar]

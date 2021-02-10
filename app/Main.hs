a42c9a540d0c8a5b7b2dc1f2425434e30e3c8d14
shibe23 <shibe23.info@gmail.com>
2021-02-10 23:34:03 +0900

    四則演算 - 足し算

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,15 @@
 import Text.Parsec
 
+expr = do
+  x <- number
+  char '+'
+  y <- number
+  return [x, y]
+
 number = do
   x <-  many1 digit
   return (read x :: Int)
 
 main = do
     parseTest number "123"
+    parseTest expr "1+2"
